1590a3c4ebf6cd322a521d4870607b959b331375
AceJester90 <94662304+AceJester90@users.noreply.github.com>
2023-02-18 01:59:55 +0800

    Update AddandSendScript.js

--- scripts/test/AddandSendScript.js
+++ scripts/test/AddandSendScript.js
@@ -1,14 +1,14 @@
-const XLSX = require("xlsx");
-const { ethers } = require("ethers");
-
+const hre = require("hardhat");
+const { ethers } = hre
+require("dotenv").config()
+const prompt = require('prompt-sync')();
 
 const provider = new ethers.providers.JsonRpcProvider("https://polygon-rpc.com");
-const signer = provider.getSigner();
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const signer = wallet.provider.getSigner(wallet.address);
 const contractAddress = "0x93411A87E32D107dE24ba30966fa0E556b499e03";
 const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256[]","name":"ids","type":"uint256[]"},{"indexed":false,"internalType":"uint256[]","name":"values","type":"uint256[]"}],"name":"TransferBatch","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"id","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"TransferSingle","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"value","type":"string"},{"indexed":true,"internalType":"uint256","name":"id","type":"uint256"}],"name":"URI","type":"event"},{"inputs":[{"internalType":"address","name":"toAdd","type":"address"},{"internalType":"bool","name":"revertOnFail","type":"bool"}],"name":"addAddress","outputs":[{"internalType":"bool","name":"","type":"bool"},{"internalType":"string","name":"errMsg","type":"string"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"addresses","outputs":[{"internalType":"uint256","name":"length","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"addresses_length","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"}],"name":"addresses_list","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_address","type":"address"}],"name":"addresses_listInv","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address[]","name":"accounts","type":"address[]"},{"internalType":"uint256[]","name":"ids","type":"uint256[]"}],"name":"balanceOfBatch","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"contractURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"}],"name":"exists","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"extrasHolder","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMFS_length","outputs":[{"internalType":"uint256","name":"length","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"ID","type":"uint256"}],"name":"getMFS_list","outputs":[{"internalType":"address","name":"FoundingSettlerAddress","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"FoundingSettlerAddress","type":"address"}],"name":"getMFS_listInv","outputs":[{"internalType":"uint256","name":"addressID","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"initialSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"toRemove","type":"address"},{"internalType":"bool","name":"revertOnFail","type":"bool"}],"name":"removeAddress","outputs":[{"internalType":"bool","name":"","type":"bool"},{"internalType":"string","name":"errMsg","type":"string"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"royaltyInfo","outputs":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"uint256","name":"royaltyAmount","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"royaltyRecipient","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256[]","name":"ids","type":"uint256[]"},{"internalType":"uint256[]","name":"amounts","type":"uint256[]"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeBatchTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"ticketID","type":"uint256"}],"name":"sendTicket","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"newUri","type":"string"}],"name":"setContractUri","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"feeInBasisPoints","type":"uint256"}],"name":"setRoyaltyInfo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"uint256","name":"id","type":"uint256"}],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"uri","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}];
-const contract = new ethers.Contract(contractAddress, abi, provider);
-const contractWithSigners = contract.connect(signer);
-const prompt = require('prompt-sync')();
+const options = { gasLimit: 500000 };
 
 function sleep(ms) {
     return new Promise((resolve) => {
@@ -16,44 +16,11 @@ function sleep(ms) {
     });
 }
 
-async function _addAddress() {
-
-    const Address = prompt('Enter Address:');
-    const time = 2.2;
-    const add = await contractWithSigners.addAddress(Address, true);
-    console.log(add);    
-    await sleep(1000*time);
-
-}
-
-async function _sendTicket() {
-
-    const Address = prompt('Enter Address:');
-    const TicketID = prompt('Enter ID:');
-    const time = 2.2;
-    const send = await contractWithSigners.sendTicket(Address, TicketID);
-    console.log(send);    
-    await sleep(1000*time);
-
-}
-
-async function _sendandadd() {
-
-    const Address = prompt('Enter Address:');
-    const TicketID = prompt('Enter ID:');
-    const time = 2.2;
-    const send = await contractWithSigners.sendTicket(Address, TicketID);
-    const add = await contractWithSigners.addAddress(Address, true);
-    console.log(send);   
-    console.log(add);   
-
-}
-
 async function main() {
+
     let option;
     while (option!=='4') {
-        const time = 2.2;
-        await sleep(1000*time);
+
         console.log('Menu');
         console.log('1: Add Address');
         console.log('2: Send Ticket');
@@ -61,13 +28,48 @@ async function main() {
         console.log('4: Exit');
         option = prompt('Enter your option:')
         if (option == 1) {
-            _addAddress();
+
+            const Contract = await hre.ethers.getContractFactory("SettlersTickets");
+            const SettlersTickets = await Contract.deploy();
+            await SettlersTickets.deployed();
+            console.log("Deployed contract to:", SettlersTickets.address);
+            const time = 4;
+            await sleep(1000*time);
+            const Address = prompt('Enter Address:');
+            const add = await SettlersTickets.addAddress(Address, true);
+            console.log(add);    
+            await sleep(1000*time);
+
         }
         else if (option == 2) {
-            _sendTicket();
+
+            const Contract = await hre.ethers.getContractFactory("SettlersTickets");
+            const SettlersTickets = await Contract.deploy();
+            await SettlersTickets.deployed();
+            console.log("Deployed contract to:", SettlersTickets.address);
+            const time = 2.2;
+            await sleep(1000*time);
+            const Address = prompt('Enter Address:');
+            const TicketID = prompt('Enter ID:');
+            const send = await SettlersTickets.sendTicket(Address, TicketID, options);
+            console.log(send);   
+
         }
         else if (option == 3) {
-            _sendandadd();
+
+            const Contract = await hre.ethers.getContractFactory("SettlersTickets");
+            const SettlersTickets = await Contract.deploy();
+            await SettlersTickets.deployed();
+            console.log("Deployed contract to:", SettlersTickets.address);
+            const time = 2;
+            await sleep(1000*time);
+            const Address = prompt('Enter Address:');
+            const TicketID = prompt('Enter ID:');
+            const send = await SettlersTickets.sendTicket(Address, TicketID, options);
+            const add = await SettlersTickets.addAddress(Address, true);
+            console.log(send);   
+            console.log(add);   
+
         }
         }
     }
